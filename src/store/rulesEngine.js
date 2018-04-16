ae80185409e195d7964394404f7ab2987ab5c95f
qubbit <gopala@email.arizona.edu>
2018-04-16 19:32:03 -0400

    Add more options to rules engine store

--- src/store/rulesEngine.js
+++ src/store/rulesEngine.js
@@ -1,27 +1,32 @@
+// A ruleset can be stored in a database, how do we want to key that ruleset?
+// For this purpose we will choose one of the facts
+
 const initialState = {
   name: 'Response Modifier',
+  config: { findBy: 'epa_type' },
   rules: [],
   facts: [
     {
       name: 'epa_type',
-      label: 'EPA type',
+      label: 'EPA Type',
       type: 'string',
       allowMultiple: true,
+      required: true,
     },
     {
       name: 'reason_code',
-      label: 'reason code',
+      label: 'Reason Code',
       type: 'string',
       allowMultiple: true,
     },
-    { name: 'note', label: 'PA note', type: 'string', allowMultiple: false },
+    { name: 'note', label: 'PA Note', type: 'string', allowMultiple: false },
   ],
   actions: {
     convert_to_fax: {
-      label: 'convert to fax',
+      label: 'Convert to fax',
       params: [{ name: 'message', type: 'string' }],
     },
-    fail_touch: { label: 'add a fail touch', params: [] },
+    fail_touch: { label: 'Add a fail touch', params: [] },
   },
   values: {
     epa_type: ['ESI', 'Anthem', 'Aetna'],
@@ -31,7 +36,8 @@ const initialState = {
   selectedAction: '',
 };
 
-// initialState.remainingFacts = initialState.facts;
+// ^ We may want to decouple selectedValues and selectedAction from the
+// rules engine because that's the concern of the UI
 
 export function changeFact(fact) {
   return dispatch => dispatch({ type: 'FACT_CHANGED', fact });
