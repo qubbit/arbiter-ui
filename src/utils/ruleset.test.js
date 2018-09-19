5773547d2a013089cf4fc9e916bd74963ce45199
qubbit <gopala@email.arizona.edu>
2018-09-19 19:06:27 -0400

    Make it possible to generate rules for arbiter

--- src/utils/ruleset.test.js
+++ src/utils/ruleset.test.js
@@ -1,9 +1,9 @@
 import * as ruleset from './ruleset';
-import r from './ruleset.json';
+import r from './ruleset2.json';
 
 describe('.transformRuleset', () => {
   it('finds correct rule in an ruleset', () => {
     var x = ruleset.transformRuleset(r);
-    // console.log(x);
+    console.log(JSON.stringify(x, null, 2));
   });
 });
